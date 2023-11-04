111f44b1b9cb5f3b42212125987c687f78edbe9f
ph_hi <ph_hi@DESKTOP-N6IF84E>
2023-11-04 16:02:38 -0300

    Add project files.

---
+++ BlazorShop.Api/Program.cs
@@ -0,0 +1,46 @@
+using BlazorShop.Api.Context;
+using BlazorShop.Api.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<ICarrinhoCompraRepository, CarrinhoCompraRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy => policy.WithOrigins("https://localhost:7205", "https://localhost:7205")
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithHeaders(HeaderNames.ContentType)
+    );
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ BlazorShop.Web/Pages/Catalogo.razor.cs
@@ -0,0 +1,14 @@
+﻿using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.Web.Pages
+{
+    public partial class Catalogo
+    {
+        public IEnumerable<ProdutoDto>? Produtos { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Produtos = await produtoService.GetItens();
+        }
+    }
+}
